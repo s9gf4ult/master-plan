8444fc5896013de3c5739f27fcd2ffac067a74bf
Alexey Uimanov <s9gf4ult@gmail.com>
2018-01-15 13:13:30 +0500

    some fixes for tests

--- test/MasterPlan/Project/GraphSpec.hs
+++ test/MasterPlan/Project/GraphSpec.hs
@@ -1,8 +1,11 @@
 module MasterPlan.Project.GraphSpec where
 
 import Control.Applicative as A
+import Control.Monad
 import Data.Foldable
 import Data.List as L
+import Data.Map.Strict as M
+import Data.Monoid
 import Data.Set as S
 import MasterPlan.Project.Graph
 import Test.Hspec
@@ -11,8 +14,8 @@ import Test.SmallCheck
 import Test.SmallCheck.Series
 
 
-propDoubleSplit :: [(Int, Int)] -> Either Reason Reason
-propDoubleSplit as = do
+propDoubleSplit :: [GraphTuple] -> Either Reason Reason
+propDoubleSplit (fmap unGraphTuple -> as) = do
   let
     h = S.fromList as
     els = S.toList $ S.fromList
@@ -24,24 +27,35 @@ propDoubleSplit as = do
   for_ subCcs $ \cc -> case L.length cc of
     1 -> return ()
     _ -> Left $ show cc
+  let
+    merged = M.unionsWith S.union $ concat subCcs
+    originalMerged = M.unionsWith S.union $ unConnectedComponent <$> ccs
+  unless (merged == originalMerged)
+    $ Left $ "merged: " <> show merged <> " but original was: " <> show originalMerged
+  let
+    splittedAgain = S.fromList $ graphComponents merged
+    originalSplitted = S.fromList $ unConnectedComponent <$> ccs
+  unless (splittedAgain == originalSplitted)
+    $ Left $ "splitted again: " <> show splittedAgain <> " but orignal: " <> show originalSplitted
   return "Ok"
 
-depTuple :: (Monad m) => Series m (Int, Int)
-depTuple = generate $ \d ->
-  if d >= 0 then go d else A.empty
-  where
-    go d = L.take 10 $ do
-      (a:as) <- L.tails $ L.reverse [0..d]
-      b <- as
-      return (a, b)
+newtype GraphTuple = GraphTuple
+  { unGraphTuple :: (Int, Int)
+  } deriving (Eq, Show)
 
-genInts :: (Monad m) => Series m [(Int, Int)]
-genInts = cons0 [] \/ intsCons
-  where
-    intsCons = decDepth $ (:) <$> depTuple <~> genInts
+instance (Monad m) => Serial m GraphTuple where
+  series = generate $ \d ->
+    if d >= 0 then go d else A.empty
+    where
+      go d = do
+        let x = min d 5
+        (a:as) <- L.tails $ [0..x]
+        b <- as
+        return $ GraphTuple (a, b)
 
 spec :: Spec
 spec = describe "Graph spec" $ do
-  it "No double split" $ property
-    $ changeDepth (const 6)
-    $ over genInts propDoubleSplit
+  describe "Props" $ do
+    it "No double split" $ property
+      $ changeDepth (const 6)
+      $ forAll propDoubleSplit
